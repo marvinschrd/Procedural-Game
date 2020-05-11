0b99b6bfc2ba48411600dac6a3808ac5e6528eaf
Marvin <marvin.k.scharding@gmail.com>
2020-05-11 23:05:24 +0200

    Added ennemy version of some ennemies

---
+++ Assets/FireBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    [SerializeField] float destructionTime = 3;
+    [SerializeField] float damage = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, destructionTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("ennemy"))
+        {
+            Health health = collision.gameObject.GetComponent<Health>();
+            health.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/RangedAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedAttack : MonoBehaviour
+{
+    [SerializeField] Transform firingPosition;
+    [SerializeField] GameObject FireBallPrefab;
+    [SerializeField] float bulletForce = 5;
+
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            animator.SetTrigger("Attacking");
+            GameObject Fireball;
+            Fireball = Instantiate(FireBallPrefab, firingPosition.position, Quaternion.identity);
+            Rigidbody2D fireBallBody = Fireball.GetComponent<Rigidbody2D>();
+            fireBallBody.AddForce(firingPosition.right * bulletForce, ForceMode2D.Impulse);
+        }
+    }
+}
